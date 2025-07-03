52112e683059d72e6d22f70ea3ade06ebd22c11d
23AD159 <153845833+23AD159@users.noreply.github.com>
2025-07-03 11:19:23 +0530

    Add files via upload
    TAsk 1

---
+++ day1/spring/src/main/java/org/example/SpringBoot.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class SpringBoot {
+    SpringBoot(){
+        System.out.println("Inside SpringBoot constructor");
+    }
+    public  void hello(String name){
+        System .out.println("Welcome to SpringBoot ");
+
+    }
+}
---
+++ day1/spring/src/main/java/org/example/Student.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Student {
+
+    public void internshipProgram(){
+        System.out.println("Students are attending class in code studio");
+    }
+
+}

8bfab5c0ef77676d4d0063bbbe56c282ec2732ea
23AD159 <153845833+23AD159@users.noreply.github.com>
2025-07-17 23:01:58 +0530

    Delete day13/task directory

--- day13/task/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-const Footer = () => (
-  <footer className="bg-secondary text-white text-center p-3 mt-auto">
-    <p>&copy; 2025 Employee Management System</p>
-  </footer>
-);
-
-export default Footer;
